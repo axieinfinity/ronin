f3eeef6bdc046aa0d7d1db8f48dc0217b9187b84
Andy Ho <hosyvietanh@gmail.com>
2020-03-05 17:55:38 +0700

    Add consortium consensus (#1)
    * Define Consortium config
    
    * Init consortium code
    
    * Verify basic header fields
    
    * Remove snapshot in consortium
    
    * Adjust VerifySeal
    
    * Adjust Prepare
    
    * Make consortium compilable
    
    * Implement some missing functions
    
    * Provide a way to get validators from contract
    
    * Authorize mining for consortium
    
    * Define genesis state for consortium
    
    * Make consortium compliance with consensus assumption

--- eth/backend.go
+++ eth/backend.go
@@ -31,7 +31,9 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/consensus/clique"
+	"github.com/ethereum/go-ethereum/consensus/consortium"
 	"github.com/ethereum/go-ethereum/consensus/ethash"
+	validatorContract "github.com/ethereum/go-ethereum/contracts/validator/contract"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/bloombits"
 	"github.com/ethereum/go-ethereum/core/rawdb"
@@ -40,6 +42,7 @@ import (
 	"github.com/ethereum/go-ethereum/eth/downloader"
 	"github.com/ethereum/go-ethereum/eth/filters"
 	"github.com/ethereum/go-ethereum/eth/gasprice"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/ethereum/go-ethereum/internal/ethapi"
@@ -252,6 +255,23 @@ func CreateConsensusEngine(ctx *node.ServiceContext, chainConfig *params.ChainCo
 	// If proof-of-authority is requested, set it up
 	if chainConfig.Clique != nil {
 		return clique.New(chainConfig.Clique, db)
+	}
+
+	if chainConfig.Consortium != nil {
+		return consortium.New(chainConfig.Consortium, db, func() ([]common.Address, error) {
+			client, err := ethclient.Dial(ctx.Config.IPCEndpoint())
+			if err != nil {
+				return nil, err
+			}
+
+			addr := common.HexToAddress(common.ValidatorSC)
+			// TODO: Consider getting from stateDB for faster performance
+			contract, err := validatorContract.NewValidator(addr, client)
+			if err != nil {
+				return nil, err
+			}
+			return contract.GetValidators(nil)
+		})
 	}
 	// Otherwise assume proof-of-work
 	switch config.PowMode {
@@ -470,6 +490,14 @@ func (s *Ethereum) StartMining(threads int) error {
 				return fmt.Errorf("signer missing: %v", err)
 			}
 			clique.Authorize(eb, wallet.SignData)
+		}
+		if consortium, ok := s.engine.(*consortium.Consortium); ok {
+			wallet, err := s.accountManager.Find(accounts.Account{Address: eb})
+			if wallet == nil || err != nil {
+				log.Error("Etherbase account unavailable locally", "err", err)
+				return fmt.Errorf("signer missing: %v", err)
+			}
+			consortium.Authorize(eb, wallet.SignData)
 		}
 		// If mining is started, we can disable the transaction rejection mechanism
 		// introduced to speed sync times.
