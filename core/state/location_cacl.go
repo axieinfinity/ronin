c4754caf11f84a6c7c9057a4aa09dd8a1587b4ed
Andy Ho <hosyvietanh@gmail.com>
2020-03-20 11:25:07 +0700

    Read statedb to check for deployer

---
+++ core/state/location_cacl.go
@@ -0,0 +1,34 @@
+package state
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func GetLocSimpleVariable(slot uint64) common.Hash {
+	slotHash := common.BigToHash(new(big.Int).SetUint64(slot))
+	return slotHash
+}
+
+func GetLocMappingAtKey(key common.Hash, slot uint64) common.Hash {
+	slotHash := common.BigToHash(new(big.Int).SetUint64(slot))
+	retByte := crypto.Keccak256(key.Bytes(), slotHash.Bytes())
+	ret := new(big.Int)
+	ret.SetBytes(retByte)
+	return common.BigToHash(ret)
+}
+
+func GetLocDynamicArrAtElement(slotHash common.Hash, index uint64, elementSize uint64) common.Hash {
+	slotKecBig := crypto.Keccak256Hash(slotHash.Bytes()).Big()
+	//arrBig = slotKecBig + index * elementSize
+	arrBig := slotKecBig.Add(slotKecBig, new(big.Int).SetUint64(index*elementSize))
+	return common.BigToHash(arrBig)
+}
+
+func GetLocFixedArrAtElement(slot uint64, index uint64, elementSize uint64) common.Hash {
+	slotBig := new(big.Int).SetUint64(slot)
+	arrBig := slotBig.Add(slotBig, new(big.Int).SetUint64(index*elementSize))
+	return common.BigToHash(arrBig)
+}
