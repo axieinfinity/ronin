b6b8771309bb67a6e47b625689870c08ff0afb15
Andy Ho <hosyvietanh@gmail.com>
2020-06-25 16:47:23 +0700

    Provide API for consortium

--- docker/chainnode/entrypoint.sh
+++ docker/chainnode/entrypoint.sh
@@ -7,19 +7,16 @@
 # - VERBOSITY (default to 3)
 # - SYNC_MODE (default to 'full')
 # - NETWORK_ID (default to 2021)
+# - GASPRICE (default to 0)
 
 # constants
-DATA_DIR="/ethereum/data"
-KEYSTORE_DIR="/ethereum/keystore"
+DATA_DIR="/ronin/data"
+KEYSTORE_DIR="/ronin/keystore"
+PASSWORD_FILE="$KEYSTORE_DIR/password"
 
 # variables
 genesisPath=""
 params=""
-accountsCount=$(
-  geth account list --datadir $DATA_DIR  --keystore $KEYSTORE_DIR \
-  2> /dev/null \
-  | wc -l
-)
 
 # networkid
 if [[ ! -z $NETWORK_ID ]]; then
@@ -29,10 +26,11 @@ if [[ ! -z $NETWORK_ID ]]; then
       ;;
     2021 )
       genesisPath="testnet.json"
-      params="$params --gcmode archive --rpcapi db,eth,net,web3,debug"
+      params="$params --gcmode archive --http.api eth,net,web3,debug,consortium"
       ;;
     2022 )
       genesisPath="devnet.json"
+      params="$params --gcmode archive --http.api eth,net,web3,debug,consortium"
       ;;
     * )
       echo "network id not supported"
@@ -47,21 +45,28 @@ if [[ ! -z $GENESIS_PATH ]]; then
 fi
 
 # data dir
-if [[ ! -d $DATA_DIR/geth ]]; then
+if [[ ! -d $DATA_DIR/ronin ]]; then
   echo "No blockchain data, creating genesis block."
-  geth init $genesisPath --datadir $DATA_DIR 2> /dev/null
+  ronin init $genesisPath --datadir $DATA_DIR 2> /dev/null
 fi
 
 # password file
-if [[ ! -f ./password ]]; then
+if [[ ! -f $PASSWORD_FILE ]]; then
+  mkdir -p $KEYSTORE_DIR
   if [[ ! -z $PASSWORD ]]; then
     echo "Password env is set. Writing into file."
-    echo "$PASSWORD" > ./password
+    echo "$PASSWORD" > $PASSWORD_FILE
   else
     echo "No password set (or empty), generating a new one"
-    $(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32} > password)
+    $(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32} > $PASSWORD_FILE)
   fi
 fi
+
+accountsCount=$(
+  ronin account list --datadir $DATA_DIR  --keystore $KEYSTORE_DIR \
+  2> /dev/null \
+  | wc -l
+)
 
 # private key
 if [[ $accountsCount -le 0 ]]; then
@@ -69,22 +74,22 @@ if [[ $accountsCount -le 0 ]]; then
   if [[ ! -z $PRIVATE_KEY ]]; then
     echo "Creating account from private key"
     echo "$PRIVATE_KEY" > ./private_key
-    geth account import ./private_key \
+    ronin account import ./private_key \
       --datadir $DATA_DIR \
       --keystore $KEYSTORE_DIR \
-      --password ./password
+      --password $PASSWORD_FILE
     rm ./private_key
   else
     echo "Creating new account"
-    geth account new \
+    ronin account new \
       --datadir $DATA_DIR \
       --keystore $KEYSTORE_DIR \
-      --password ./password
+      --password $PASSWORD_FILE
   fi
 fi
 
 account=$(
-  geth account list --datadir $DATA_DIR  --keystore $KEYSTORE_DIR \
+  ronin account list --datadir $DATA_DIR  --keystore $KEYSTORE_DIR \
   2> /dev/null \
   | head -n 1 \
   | cut -d"{" -f 2 | cut -d"}" -f 1
@@ -102,9 +107,25 @@ if [[ ! -z $SYNC_MODE ]]; then
   params="$params --syncmode ${SYNC_MODE}"
 fi
 
-# debug mode
-if [[ ! -z $DEBUG_MODE ]]; then
-  params="$params --gcmode archive --rpcapi db,eth,net,web3,debug,posv"
+# debug mode - enable rpc and disable local transactions
+if [[ ! -z $RPC_NODE ]]; then
+  params="$params --gcmode archive --http.api eth,net,web3,debug,consortium --txpool.nolocals"
+fi
+
+# ethstats
+if [[ ! -z $ETHSTATS_ENDPOINT ]]; then
+  params="$params --ethstats $ETHSTATS_ENDPOINT"
+fi
+
+#nodekey
+if [[ ! -z $NODEKEY ]]; then
+  echo $NODEKEY > $PWD/.nodekey
+  params="$params --nodekey $PWD/.nodekey"
+fi
+
+#gasprice
+if [[ ! -z $GASPRICE ]]; then
+  params="$params --miner.gasprice $GASPRICE"
 fi
 
 # dump
@@ -112,23 +133,24 @@ echo "dump: $account $BOOTNODES"
 
 set -x
 
-exec geth $params \
+exec ronin $params \
   --verbosity $VERBOSITY \
   --datadir $DATA_DIR \
   --keystore $KEYSTORE_DIR \
-  --password ./password \
+  --password $PASSWORD_FILE \
   --port 30303 \
-  --rpc \
-  --rpccorsdomain "*" \
-  --rpcaddr 0.0.0.0 \
-  --rpcport 8545 \
-  --rpcvhosts "*" \
+  --txpool.globalqueue 5000 \
+  --txpool.globalslots 5000 \
+  --http \
+  --http.corsdomain "*" \
+  --http.addr 0.0.0.0 \
+  --http.port 8545 \
+  --http.vhosts "*" \
   --ws \
-  --wsaddr 0.0.0.0 \
-  --wsport 8546 \
-  --wsorigins "*" \
+  --ws.addr 0.0.0.0 \
+  --ws.port 8546 \
+  --ws.origins "*" \
   --mine \
   --allow-insecure-unlock \
-  --gasprice "0" \
-  --targetgaslimit "20000000" \
+  --miner.gastarget "100000000" \
   "$@"
