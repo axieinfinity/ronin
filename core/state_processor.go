e0201cb049464acfc36d83c047d21f35922d45bb
Andy Ho <hosyvietanh@gmail.com>
2020-06-26 17:03:27 +0700

    Only check whitelist deployer in case of consoritum

--- core/state_processor.go
+++ core/state_processor.go
@@ -94,7 +94,7 @@ func ApplyTransaction(config *params.ChainConfig, bc ChainContext, author *commo
 	}
 
 	// Check if sender is authorized to deploy
-	if msg.To() == nil {
+	if msg.To() == nil && config.Consortium != nil {
 		whitelistedDeployer := state.IsWhitelistedDeployer(statedb, msg.From())
 		if !whitelistedDeployer {
 			return nil, errors.New("unauthorized deployer")
