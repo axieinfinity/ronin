4376fb31185996cb3e0c400fbc6ed8cde46de389
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-06-22 15:43:02 +0000

    Fix typo

--- docker/chainnode/entrypoint.sh
+++ docker/chainnode/entrypoint.sh
@@ -24,7 +24,7 @@ if [[ ! -z $NETWORK_ID ]]; then
   case $NETWORK_ID in
     2020 )
       genesisPath="mainnet.json"
-      params="$parmas $RONIN_PARAMS"
+      params="$params $RONIN_PARAMS"
       ;;
     2021 )
       genesisPath="testnet.json"
