ec4a43d712ed292bb370b03b1cdafe0743daf937
Duc Tho Tran <ducthotran2010@gmail.com>
2021-06-22 13:21:31 +0700

    Increase tx pool size

--- docker/chainnode/entrypoint.sh
+++ docker/chainnode/entrypoint.sh
@@ -140,7 +140,7 @@ exec ronin $params \
   --password $PASSWORD_FILE \
   --port 30303 \
   --txpool.globalqueue 5000 \
-  --txpool.globalslots 5000 \
+  --txpool.globalslots 10000 \
   --http \
   --http.corsdomain "*" \
   --http.addr 0.0.0.0 \
