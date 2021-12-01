98b2003bb8af989b65ec6e5d2a279712c6556ae8
kien Dang <kien.15@gmail.com>
2021-12-01 10:31:23 +0700

    Increase wiggleTime to 1s

--- consensus/consortium/consortium.go
+++ consensus/consortium/consortium.go
@@ -48,7 +48,7 @@ const (
 	inmemorySnapshots  = 128  // Number of recent vote snapshots to keep in memory
 	inmemorySignatures = 4096 // Number of recent block signatures to keep in memory
 
-	wiggleTime = 500 * time.Millisecond // Random delay (per signer) to allow concurrent signers
+	wiggleTime = 1000 * time.Millisecond // Random delay (per signer) to allow concurrent signers
 )
 
 // Consortium proof-of-authority protocol constants.
