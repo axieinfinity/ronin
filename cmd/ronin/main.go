0e703922ce64efeb40f3fb065a3c97ee04e4af47
Martin Holst Swende <martin@swende.se>
2021-12-11 16:51:05 +0100

    cmd/geth, console: support interrupting the js console (#23387)
    Previously, Ctrl-C (SIGINT) was ignored during JS execution, so it was not
    possible to get out of infinite loops in the console. With this change,
    Ctrl-C now interrupts JS.
    
    Fixes #23344
    
    Co-authored-by: Sina Mahmoodi <itz.s1na@gmail.com>
    Co-authored-by: Felix Lange <fjl@twurst.com>

--- cmd/ronin/main.go
+++ cmd/ronin/main.go
@@ -355,7 +355,7 @@ func geth(ctx *cli.Context) error {
 		<-subs.Start()
 	}
 
-	startNode(ctx, stack, backend)
+	startNode(ctx, stack, backend, false)
 	stack.Wait()
 	return nil
 }
@@ -363,11 +363,11 @@ func geth(ctx *cli.Context) error {
 // startNode boots up the system node and all registered protocols, after which
 // it unlocks any requested accounts, and starts the RPC/IPC interfaces and the
 // miner.
-func startNode(ctx *cli.Context, stack *node.Node, backend ethapi.Backend) {
+func startNode(ctx *cli.Context, stack *node.Node, backend ethapi.Backend, isConsole bool) {
 	debug.Memsize.Add("node", stack)
 
 	// Start up the node itself
-	utils.StartNode(ctx, stack)
+	utils.StartNode(ctx, stack, isConsole)
 
 	// Unlock any account specifically requested
 	unlockAccounts(ctx, stack)
