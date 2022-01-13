91f8e8344ba356228dfa71da3c9c3a4ccecd6ba0
Quang Le <ducquang98@gmail.com>
2022-01-13 08:56:15 +0100

    add testing

--- metrics/period_counter.go
+++ metrics/period_counter.go
@@ -1,7 +1,6 @@
 package metrics
 
 import (
-	"sync/atomic"
 	"time"
 )
 
@@ -12,23 +11,46 @@ const (
 	monthlyPeriodType  = iota
 )
 
+var Now = time.Now
+
 type PeriodCounter struct {
-	StandardCounter
+	*StandardCounter
 	PeriodType      int
 	StoredPeriod    int
 	CounterResetted bool
 }
 
+func (c *PeriodCounter) SetPeriodType(t int) {
+	c.PeriodType = t
+}
+
 func (c *PeriodCounter) Inc(i int64) {
 	c.ResetIfBeginningOfPeriod()
-	atomic.AddInt64(&c.count, i)
+	c.StandardCounter.Inc(i)
 }
 
 func (c *PeriodCounter) Dec(i int64) {
 	c.ResetIfBeginningOfPeriod()
-	atomic.AddInt64(&c.count, -i)
+	c.StandardCounter.Dec(i)
 }
 
+// Clear sets the counter to zero.
+func (c *PeriodCounter) Clear() {
+	c.StandardCounter.Clear()
+}
+
+// Count returns the current count.
+func (c *PeriodCounter) Count() int64 {
+	return c.StandardCounter.Count()
+}
+
+// Snapshot returns a read-only copy of the counter.
+func (c *PeriodCounter) Snapshot() Counter {
+	return c.StandardCounter.Snapshot()
+}
+
+// if current time is a new period compared to stored period, and counter is not resetted yet
+// then reset the counter
 func (c *PeriodCounter) ResetIfBeginningOfPeriod() {
 	if c.CurrentPeriod() != c.StoredPeriod && !c.CounterResetted {
 		c.CounterResetted = true
@@ -39,17 +61,38 @@ func (c *PeriodCounter) ResetIfBeginningOfPeriod() {
 	}
 }
 
+// return the period value of current time.
+// For example, if period type is minutelyPeriodType and current time is 12:03,
+// it should return 3. If period type is monthlyPeriodType and current datetime is 3/12
+// it should return 12
 func (c *PeriodCounter) CurrentPeriod() int {
 	switch c.PeriodType {
 	case minutelyPeriodType:
-		return time.Now().Minute()
+		return Now().Minute()
 	case hourlyPeriodType:
-		return time.Now().Hour()
+		return Now().Hour()
 	case dailyPeriodType:
-		return time.Now().Day()
+		return Now().Day()
 	case monthlyPeriodType:
-		return int(time.Now().Month())
+		return int(Now().Month())
 	default:
 		panic("invalid period type")
 	}
+}
+
+// NewPeriodCounter constructs a new PeriodCounter.
+func NewPeriodCounter() Counter {
+	if !Enabled {
+		return NilCounter{}
+	}
+	return &PeriodCounter{&StandardCounter{0}, hourlyPeriodType, 0, false}
+}
+
+// GetOrRegisterPeriodCounter returns an existing Counter or constructs and registers
+// a new PeriodCounter.
+func GetOrRegisterPeriodCounter(name string, r Registry) Counter {
+	if nil == r {
+		r = DefaultRegistry
+	}
+	return r.GetOrRegister(name, NewPeriodCounter).(Counter)
 }
--- metrics/period_counter_test.go
+++ metrics/period_counter_test.go
@@ -1,1 +1,26 @@
 package metrics
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCurrentPeriod(t *testing.T) {
+	Now = func() time.Time { return time.Date(2021, time.Month(2), 21, 1, 10, 30, 0, time.UTC) }
+
+	counter := NewPeriodCounter()
+	periodCounter := counter.(*PeriodCounter)
+	periodCounter.PeriodType = minutelyPeriodType
+	assert.Equal(t, periodCounter.CurrentPeriod(), 10)
+
+	periodCounter.PeriodType = hourlyPeriodType
+	assert.Equal(t, periodCounter.CurrentPeriod(), 1)
+
+	periodCounter.PeriodType = dailyPeriodType
+	assert.Equal(t, periodCounter.CurrentPeriod(), 21)
+
+	periodCounter.PeriodType = monthlyPeriodType
+	assert.Equal(t, periodCounter.CurrentPeriod(), 2)
+}
