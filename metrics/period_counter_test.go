48d180cb68d4b3f3598868964edc86721004f470
Quang Le <ducquang98@gmail.com>
2022-01-13 09:47:59 +0100

    add specs

--- metrics/period_counter_test.go
+++ metrics/period_counter_test.go
@@ -24,3 +24,44 @@ func TestCurrentPeriod(t *testing.T) {
 	periodCounter.PeriodType = monthlyPeriodType
 	assert.Equal(t, periodCounter.CurrentPeriod(), 2)
 }
+
+func TestResetIfBeginningOfPeriod(t *testing.T) {
+	Now = func() time.Time { return time.Date(2021, time.Month(2), 21, 1, 10, 30, 0, time.UTC) }
+
+	counter := NewPeriodCounter()
+	periodCounter := counter.(*PeriodCounter)
+	periodCounter.PeriodType = minutelyPeriodType
+	currentPeriod := periodCounter.CurrentPeriod()
+
+	periodCounter.StoredPeriod = currentPeriod - 1
+	periodCounter.ResetIfBeginningOfPeriod()
+
+	assert.Equal(t, periodCounter.StandardCounter.count, int64(0))
+	assert.Equal(t, periodCounter.StoredPeriod, currentPeriod)
+	assert.True(t, periodCounter.CounterResetted)
+
+	periodCounter.StandardCounter.count = 2
+	periodCounter.ResetIfBeginningOfPeriod()
+	assert.Equal(t, periodCounter.StandardCounter.count, int64(2))
+
+	oldPeriod := currentPeriod - 2
+	periodCounter.StoredPeriod = oldPeriod
+	periodCounter.ResetIfBeginningOfPeriod()
+
+	assert.Equal(t, periodCounter.StandardCounter.count, int64(0))
+	assert.Equal(t, periodCounter.StoredPeriod, currentPeriod)
+	assert.True(t, periodCounter.CounterResetted)
+
+	periodCounter.StandardCounter.count = 1
+	periodCounter.StoredPeriod = oldPeriod
+	periodCounter.ResetIfBeginningOfPeriod()
+
+	assert.Equal(t, periodCounter.StandardCounter.count, int64(1))
+	assert.Equal(t, periodCounter.StoredPeriod, oldPeriod)
+	assert.False(t, periodCounter.CounterResetted)
+
+	periodCounter.ResetIfBeginningOfPeriod()
+	assert.Equal(t, periodCounter.StandardCounter.count, int64(0))
+	assert.Equal(t, periodCounter.StoredPeriod, currentPeriod)
+	assert.True(t, periodCounter.CounterResetted)
+}
