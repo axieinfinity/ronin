11c94a26ee775853111beef2565467f646ce2452
Linh Tran <110378492+linh-1@users.noreply.github.com>
2022-08-12 14:45:02 +0700

    [RON-161] feat: implement function to interacting with smart contract (#94)
    * feat(consortiumV2): implement function to interacting with smart contract directly
    
    * fix: missing new parameters
    
    * fix: missing new parameters
    
    * fix: invalid check consortium config nil
    
    * fix: use signer hash instead of hash method

---
+++ consensus/consortium/v2/contract.go
@@ -0,0 +1,284 @@
+package v2
+
+import (
+	"bytes"
+	"context"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/consensus"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/systemcontracts"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/internal/ethapi"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/ethereum/go-ethereum/rpc"
+	"math"
+	"math/big"
+)
+
+// Interaction with contract/account
+func (c *Consortium) getCurrentValidators(blockHash common.Hash, blockNumber *big.Int) ([]common.Address, error) {
+	blockNr := rpc.BlockNumberOrHashWithHash(blockHash, false)
+	method := "getValidators"
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel() // cancel when we are finished consuming integers
+
+	data, err := c.validatorSetABI.Pack(method)
+	if err != nil {
+		log.Error("Unable to pack tx for getValidators", "error", err)
+		return nil, err
+	}
+
+	// call
+	msgData := (hexutil.Bytes)(data)
+	toAddress := common.HexToAddress(systemcontracts.ValidatorContract)
+	gas := (hexutil.Uint64)(uint64(math.MaxUint64 / 2))
+	result, err := c.ethAPI.Call(ctx, ethapi.TransactionArgs{
+		Gas:  &gas,
+		To:   &toAddress,
+		Data: &msgData,
+	}, blockNr, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		ret0 = new([]common.Address)
+	)
+	out := ret0
+
+	if err := c.validatorSetABI.UnpackIntoInterface(out, method, result); err != nil {
+		return nil, err
+	}
+
+	valz := make([]common.Address, len(*ret0))
+	for i, a := range *ret0 {
+		valz[i] = a
+	}
+	return valz, nil
+}
+
+// slash spoiled validators
+func (c *Consortium) distributeIncoming(
+	val common.Address,
+	state *state.StateDB,
+	header *types.Header,
+	chain core.ChainContext,
+	txs *[]*types.Transaction,
+	receipts *[]*types.Receipt,
+	receivedTxs *[]*types.Transaction,
+	usedGas *uint64,
+	mining bool,
+) error {
+	coinbase := header.Coinbase
+	balance := state.GetBalance(consensus.SystemAddress)
+	if balance.Cmp(common.Big0) <= 0 {
+		return nil
+	}
+	state.SetBalance(consensus.SystemAddress, big.NewInt(0))
+	state.AddBalance(coinbase, balance)
+
+	log.Trace("distribute to validator contract", "block hash", header.Hash(), "amount", balance)
+	return c.distributeToValidator(balance, val, state, header, chain, txs, receipts, receivedTxs, usedGas, mining)
+}
+
+func (c *Consortium) initContract(
+	state *state.StateDB,
+	header *types.Header,
+	chain core.ChainContext,
+	txs *[]*types.Transaction,
+	receipts *[]*types.Receipt,
+	receivedTxs *[]*types.Transaction,
+	usedGas *uint64,
+	mining bool,
+) error {
+	// method
+	method := "init"
+	// contracts
+	contracts := []string{
+		systemcontracts.ValidatorContract,
+		systemcontracts.SlashContract,
+	}
+	// get packed data
+	data, err := c.validatorSetABI.Pack(method)
+	if err != nil {
+		log.Error("Unable to pack tx for init validator set", "error", err)
+		return err
+	}
+	for _, contract := range contracts {
+		msg := c.getSystemMessage(header.Coinbase, common.HexToAddress(contract), data, common.Big0)
+		// apply message
+		log.Trace("init contract", "block hash", header.Hash(), "contract", contract)
+		err = c.applyTransaction(msg, state, header, chain, txs, receipts, receivedTxs, usedGas, mining)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// slash spoiled validators
+func (c *Consortium) distributeToValidator(
+	amount *big.Int,
+	validator common.Address,
+	state *state.StateDB,
+	header *types.Header,
+	chain core.ChainContext,
+	txs *[]*types.Transaction,
+	receipts *[]*types.Receipt,
+	receivedTxs *[]*types.Transaction,
+	usedGas *uint64,
+	mining bool,
+) error {
+	// method
+	method := "deposit"
+
+	// get packed data
+	data, err := c.validatorSetABI.Pack(method,
+		validator,
+	)
+	if err != nil {
+		log.Error("Unable to pack tx for deposit", "error", err)
+		return err
+	}
+	// get system message
+	msg := c.getSystemMessage(header.Coinbase, common.HexToAddress(systemcontracts.ValidatorContract), data, amount)
+	// apply message
+	return c.applyTransaction(msg, state, header, chain, txs, receipts, receivedTxs, usedGas, mining)
+}
+
+func (c *Consortium) getSystemMessage(from, toAddress common.Address, data []byte, value *big.Int) callmsg {
+	return callmsg{
+		ethereum.CallMsg{
+			From:     from,
+			Gas:      math.MaxUint64 / 2,
+			GasPrice: big.NewInt(0),
+			Value:    value,
+			To:       &toAddress,
+			Data:     data,
+		},
+	}
+}
+
+func (c *Consortium) applyTransaction(
+	msg callmsg,
+	state *state.StateDB,
+	header *types.Header,
+	chainContext core.ChainContext,
+	txs *[]*types.Transaction,
+	receipts *[]*types.Receipt,
+	receivedTxs *[]*types.Transaction,
+	usedGas *uint64,
+	mining bool,
+) (err error) {
+	nonce := state.GetNonce(msg.From())
+	expectedTx := types.NewTransaction(nonce, *msg.To(), msg.Value(), msg.Gas(), msg.GasPrice(), msg.Data())
+	expectedHash := c.signer.Hash(expectedTx)
+
+	if msg.From() == c.val && mining {
+		expectedTx, err = c.signTxFn(accounts.Account{Address: msg.From()}, expectedTx, c.chainConfig.ChainID)
+		if err != nil {
+			return err
+		}
+	} else {
+		if receivedTxs == nil || len(*receivedTxs) == 0 || (*receivedTxs)[0] == nil {
+			return errors.New("supposed to get a actual transaction, but get none")
+		}
+		actualTx := (*receivedTxs)[0]
+		if !bytes.Equal(c.signer.Hash(actualTx).Bytes(), expectedHash.Bytes()) {
+			return fmt.Errorf("expected tx hash %v, get %v, nonce %d, to %s, value %s, gas %d, gasPrice %s, data %s", expectedHash.String(), actualTx.Hash().String(),
+				expectedTx.Nonce(),
+				expectedTx.To().String(),
+				expectedTx.Value().String(),
+				expectedTx.Gas(),
+				expectedTx.GasPrice().String(),
+				hex.EncodeToString(expectedTx.Data()),
+			)
+		}
+		expectedTx = actualTx
+		// move to next
+		*receivedTxs = (*receivedTxs)[1:]
+	}
+	state.Prepare(expectedTx.Hash(), len(*txs))
+	gasUsed, err := applyMessage(msg, state, header, c.chainConfig, chainContext)
+	if err != nil {
+		return err
+	}
+	*txs = append(*txs, expectedTx)
+	var root []byte
+	if c.chainConfig.IsByzantium(header.Number) {
+		state.Finalise(true)
+	} else {
+		root = state.IntermediateRoot(c.chainConfig.IsEIP158(header.Number)).Bytes()
+	}
+	*usedGas += gasUsed
+	receipt := types.NewReceipt(root, false, *usedGas)
+	receipt.TxHash = expectedTx.Hash()
+	receipt.GasUsed = gasUsed
+
+	// Set the receipt logs and create a bloom for filtering
+	receipt.Logs = state.GetLogs(expectedTx.Hash(), header.Hash())
+	receipt.Bloom = types.CreateBloom(types.Receipts{receipt})
+	receipt.BlockHash = header.Hash()
+	receipt.BlockNumber = header.Number
+	receipt.TransactionIndex = uint(state.TxIndex())
+	*receipts = append(*receipts, receipt)
+	state.SetNonce(msg.From(), nonce+1)
+	return nil
+}
+
+func applyMessage(
+	msg callmsg,
+	state *state.StateDB,
+	header *types.Header,
+	chainConfig *params.ChainConfig,
+	chainContext core.ChainContext,
+) (uint64, error) {
+	// Create a new context to be used in the EVM environment
+	context := core.NewEVMBlockContext(header, chainContext, nil)
+	// Create a new environment which holds all relevant information
+	// about the transaction and calling mechanisms.
+	vmenv := vm.NewEVM(context, vm.TxContext{Origin: msg.From(), GasPrice: big.NewInt(0)}, state, chainConfig, vm.Config{})
+	// Apply the transaction to the current state (included in the env)
+	ret, returnGas, err := vmenv.Call(
+		vm.AccountRef(msg.From()),
+		*msg.To(),
+		msg.Data(),
+		msg.Gas(),
+		msg.Value(),
+	)
+	if err != nil {
+		log.Error("apply message failed", "msg", string(ret), "err", err)
+	}
+	return msg.Gas() - returnGas, err
+}
+
+// callmsg implements core.Message to allow passing it as a transaction simulator.
+type callmsg struct {
+	ethereum.CallMsg
+}
+
+func (m callmsg) From() common.Address { return m.CallMsg.From }
+
+func (m callmsg) Nonce() uint64 { return 0 }
+
+func (m callmsg) CheckNonce() bool { return false }
+
+func (m callmsg) To() *common.Address { return m.CallMsg.To }
+
+func (m callmsg) GasPrice() *big.Int { return m.CallMsg.GasPrice }
+
+func (m callmsg) Gas() uint64 { return m.CallMsg.Gas }
+
+func (m callmsg) Value() *big.Int { return m.CallMsg.Value }
+
+func (m callmsg) Data() []byte { return m.CallMsg.Data }
