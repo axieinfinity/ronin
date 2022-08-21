9c045abc7d8170fdde3937349bcc62a90c5f80fb
Kien Dang <kien.15@gmail.com>
2022-08-21 17:00:22 +0700

    fix: Add missing signTx params when calling `consortium.Authorize` on miner (#108)

--- eth/backend.go
+++ eth/backend.go
@@ -509,7 +509,7 @@ func (s *Ethereum) StartMining(threads int) error {
 				log.Error("Etherbase account unavailable locally", "err", err)
 				return fmt.Errorf("signer missing: %v", err)
 			}
-			consortium.Authorize(eb, wallet.SignData)
+			consortium.Authorize(eb, wallet.SignData, wallet.SignTx)
 		}
 		// If mining is started, we can disable the transaction rejection mechanism
 		// introduced to speed sync times.
