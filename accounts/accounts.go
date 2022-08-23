4a5025308809a0dc1c1e3e38a1a96c76e63d5ed7
Linh Tran <110378492+linh-1@users.noreply.github.com>
2022-08-23 15:21:33 +0700

    feat: implement Seal method (#111)

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -39,6 +39,7 @@ const (
 	MimetypeDataWithValidator = "data/validator"
 	MimetypeTypedData         = "data/typed"
 	MimetypeClique            = "application/x-clique-header"
+	MimetypeConsortium        = "application/x-clique-header"
 	MimetypeTextPlain         = "text/plain"
 )
 
--- consensus/consortium/v2/consortiumv2fork.go
+++ consensus/consortium/v2/consortiumv2fork.go
@@ -2,7 +2,27 @@ package v2
 
 import (
 	"github.com/ethereum/go-ethereum/core/types"
+	"math/rand"
+	"time"
 )
+
+const (
+	wiggleTimeBeforeFork       = 500 * time.Millisecond // Random delay (per signer) to allow concurrent signers
+	fixedBackOffTimeBeforeFork = 200 * time.Millisecond
+)
+
+func (c *Consortium) delayForConsortiumV2Fork(snap *Snapshot, header *types.Header) time.Duration {
+	delay := time.Until(time.Unix(int64(header.Time), 0)) // nolint: gosimple
+	if c.chainConfig.IsConsortiumV2(header.Number) {
+		return delay
+	}
+	if header.Difficulty.Cmp(diffNoTurn) == 0 {
+		// It's not our turn explicitly to sign, delay it a bit
+		wiggle := time.Duration(len(snap.Validators)/2+1) * wiggleTimeBeforeFork
+		delay += fixedBackOffTimeBeforeFork + time.Duration(rand.Int63n(int64(wiggle)))
+	}
+	return delay
+}
 
 func (c *Consortium) blockTimeForConsortiumV2Fork(snap *Snapshot, header, parent *types.Header) uint64 {
 	blockTime := parent.Time + c.config.Period
