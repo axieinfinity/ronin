e4ca1ebda10d3f61c5988e5d1052aaf242ba2c0c
kien Dang <kien.15@gmail.com>
2022-08-25 15:08:38 +0700

    fix: Missing recents list in snapshot while switching v1 to v2

--- consensus/consortium/common/types.go
+++ consensus/consortium/common/types.go
@@ -2,6 +2,8 @@ package common
 
 import (
 	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/core/types"
 	"math/big"
 )
@@ -11,3 +13,7 @@ import (
 type SignerFn func(accounts.Account, string, []byte) ([]byte, error)
 
 type SignerTxFn func(accounts.Account, *types.Transaction, *big.Int) (*types.Transaction, error)
+
+type ConsortiumAdapter interface {
+	GetRecents(chain consensus.ChainHeaderReader, number uint64) map[uint64]common.Address
+}
