50ad7145a0124e597bb8130e65f090bb288f7e91
Kien Dang <kien.15@gmail.com>
2022-09-05 11:09:04 +0700

    fix: reorgs and incorrect nonce in consortium-v2 (#118)
    * fix: reorgs and incorrect nonce in consortium-v2
    
    * chore: Remove getValidatorsFromLastCheckpoint, use snapshot instead

--- miner/worker.go
+++ miner/worker.go
@@ -663,7 +663,7 @@ func (w *worker) resultLoop() {
 				continue
 			}
 			log.Info("Successfully sealed new block", "number", block.Number(), "sealhash", sealhash, "hash", hash,
-				"elapsed", common.PrettyDuration(time.Since(task.createdAt)))
+				"elapsed", common.PrettyDuration(time.Since(task.createdAt)), "txs", len(block.Transactions()))
 
 			// Broadcast the block and announce chain insertion event
 			w.mux.Post(core.NewMinedBlockEvent{Block: block})
@@ -1079,7 +1079,6 @@ func (w *worker) postSideBlock(event core.ChainSideEvent) {
 
 // totalFees computes total consumed miner fees in ETH. Block transactions and receipts have to have the same order.
 func totalFees(block *types.Block, receipts []*types.Receipt) *big.Float {
-	log.Info("getting totalFees", "block", block.Number().Int64())
 	feesWei := new(big.Int)
 	for i, tx := range block.Transactions() {
 		minerFee, _ := tx.EffectiveGasTip(block.BaseFee())
