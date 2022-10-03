e7facf6d76e63bcd65bb2fe368fe57d761af6518
linh-1 <linh.tran@skymavis.com>
2022-10-03 17:26:48 +0700

    docs(consortium-main): fix typo

--- consensus/consortium/common/types.go
+++ consensus/consortium/common/types.go
@@ -14,8 +14,8 @@ type SignerFn func(accounts.Account, string, []byte) ([]byte, error)
 // SignerTxFn is a signer callback function to request the wallet to sign the given transaction.
 type SignerTxFn func(accounts.Account, *types.Transaction, *big.Int) (*types.Transaction, error)
 
-// ConsortiumAdapter defines a small collection of methods needed to access the local
-// methods between consensus engine
+// ConsortiumAdapter defines a small collection of methods needed to access the private
+// methods between consensus engines
 type ConsortiumAdapter interface {
 	GetRecents(chain consensus.ChainHeaderReader, number uint64) map[uint64]common.Address
 }
