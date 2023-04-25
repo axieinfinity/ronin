5a7331c1fe68c20b8800bed6761a044d4f902996
Exca-DK <85954505+Exca-DK@users.noreply.github.com>
2023-04-25 12:12:34 +0200

    p2p/discover: add traffic metrics (#27008)
    Co-authored-by: Exca-DK <dev@DESKTOP-RI45P4J.localdomain>
    Co-authored-by: rjl493456442 <garyrong0905@gmail.com>

--- p2p/discover/metrics.go
+++ p2p/discover/metrics.go
@@ -1,18 +1,34 @@
+// Copyright 2023 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
 package discover
 
 import (
 	"fmt"
+	"net"
 
 	"github.com/ethereum/go-ethereum/metrics"
 )
 
 const (
 	moduleName = "discover"
-
-	// ingressMeterName is the prefix of the per-packet inbound metrics
+	// ingressMeterName is the prefix of the per-packet inbound metrics.
 	ingressMeterName = moduleName + "/ingress"
 
-	// egressMeterName is the prefix of the per-packet outbound metrics
+	// egressMeterName is the prefix of the per-packet outbound metrics.
 	egressMeterName = moduleName + "/egress"
 )
 
@@ -40,4 +56,16 @@ func newMeteredConn(conn UDPConn) UDPConn {
 	return &meteredUdpConn{UDPConn: conn}
 }
 
-// Read delegates a network read to the underlying connection, bumpding the
+// Read delegates a network read to the underlying connection, bumping the udp ingress traffic meter along the way.
+func (c *meteredUdpConn) ReadFromUDP(b []byte) (n int, addr *net.UDPAddr, err error) {
+	n, addr, err = c.UDPConn.ReadFromUDP(b)
+	ingressTrafficMeter.Mark(int64(n))
+	return n, addr, err
+}
+
+// Write delegates a network write to the underlying connection, bumping the udp egress traffic meter along the way.
+func (c *meteredUdpConn) WriteToUDP(b []byte, addr *net.UDPAddr) (n int, err error) {
+	n, err = c.UDPConn.WriteToUDP(b, addr)
+	egressTrafficMeter.Mark(int64(n))
+	return n, err
+}
