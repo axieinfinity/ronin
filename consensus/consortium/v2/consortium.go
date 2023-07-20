94bacf87fee375a8df89b80c3a5d5687cc4ebd85
Bui Quang Minh <minh.bui@skymavis.com>
2023-07-20 18:19:22 +0700

    consortium/v2: add RPC API for querying finality vote easily
    This commit adds 2 APIs in consortiumv2 namespace:
    consortiumv2_getValidatorAtHash, consortiumv2_getFinalityVoteAtHash.
    consortiumv2_getValidatorAtHash returns the authorized validators that can seal
    block hash with BLS public key. consortiumv2_getFinalityVoteAtHash returns the
    finality vote information in the block header at block hash.

--- consensus/consortium/v2/consortium.go
+++ consensus/consortium/v2/consortium.go
@@ -1024,7 +1024,14 @@ func (c *Consortium) Close() error {
 
 // APIs are backward compatible with the v1, so we do not to implement it again
 func (c *Consortium) APIs(chain consensus.ChainHeaderReader) []rpc.API {
-	return []rpc.API{}
+	return []rpc.API{
+		{
+			Namespace: "consortiumv2",
+			Version:   "1.0",
+			Service:   &consortiumV2Api{chain: chain, consortium: c},
+			Public:    false,
+		},
+	}
 }
 
 // CalcDifficulty is the difficulty adjustment algorithm. It returns the difficulty
