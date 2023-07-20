d69cb58c09cabfebdb50001c185f95fe60729c5a
minh-bq <97180373+minh-bq@users.noreply.github.com>
2023-07-20 11:04:46 +0700

    blockchain, rpc, backend: add new forkchoice and finalized block query (#303)
    * core/blockchain: choose the chain with highest justified block in fork choice
    
    This commit introduces the new logic to fork choice to choose the fork with
    higher justified block before comparing total diffculty. In case the justified
    blocks in forks have the same height, we fallback to compare total difficulty
    same as before.
    
    * rpc, backend: add finalized block the block query

--- rpc/types.go
+++ rpc/types.go
@@ -58,9 +58,10 @@ type jsonWriter interface {
 type BlockNumber int64
 
 const (
-	PendingBlockNumber  = BlockNumber(-2)
-	LatestBlockNumber   = BlockNumber(-1)
-	EarliestBlockNumber = BlockNumber(0)
+	FinalizedBlockNumber = BlockNumber(-3)
+	PendingBlockNumber   = BlockNumber(-2)
+	LatestBlockNumber    = BlockNumber(-1)
+	EarliestBlockNumber  = BlockNumber(0)
 )
 
 // UnmarshalJSON parses the given JSON fragment into a BlockNumber. It supports:
@@ -84,6 +85,9 @@ func (bn *BlockNumber) UnmarshalJSON(data []byte) error {
 		return nil
 	case "pending":
 		*bn = PendingBlockNumber
+		return nil
+	case "finalized":
+		*bn = FinalizedBlockNumber
 		return nil
 	}
 
@@ -109,6 +113,8 @@ func (bn BlockNumber) MarshalText() ([]byte, error) {
 		return []byte("latest"), nil
 	case PendingBlockNumber:
 		return []byte("pending"), nil
+	case FinalizedBlockNumber:
+		return []byte("finalized"), nil
 	default:
 		return hexutil.Uint64(bn).MarshalText()
 	}
@@ -153,6 +159,10 @@ func (bnh *BlockNumberOrHash) UnmarshalJSON(data []byte) error {
 		return nil
 	case "pending":
 		bn := PendingBlockNumber
+		bnh.BlockNumber = &bn
+		return nil
+	case "finalized":
+		bn := FinalizedBlockNumber
 		bnh.BlockNumber = &bn
 		return nil
 	default:
