30e7c38058a4e6bba0ead1f9c209ef254570432b
colin <102356659+colinlyguo@users.noreply.github.com>
2024-01-19 23:43:02 +0800

    ethclient: apply accessList field in toCallArg (#28832)
    commit https://github.com/ethereum/go-ethereum/commit/1c488298c807f4daa3cbe260efb88b81902a903d.
    
    Co-authored-by: Felix Lange <fjl@twurst.com>

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -576,6 +576,9 @@ func toCallArg(msg ethereum.CallMsg) interface{} {
 	if msg.GasTipCap != nil {
 		arg["maxPriorityFeePerGas"] = (*hexutil.Big)(msg.GasTipCap)
 	}
+	if msg.AccessList != nil {
+		arg["accessList"] = msg.AccessList
+	}
 	return arg
 }
 
