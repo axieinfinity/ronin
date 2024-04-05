219e4a105209548c93285cb382cf5ab66e35e345
minh-bq <97180373+minh-bq@users.noreply.github.com>
2024-04-05 15:43:34 +0700

    consortium-v2: increase the nonce of sender before transaction execution (#434)
    Currently, in system transaction we increase nonce after transaction execution
    which is not consistent with normal transaction. This change does not require a
    hardfork as:
    - Nonce is not used when executing transaction in virtual machine
    - Consortium-v2 is after Byzantium so we don't fall through the path to
      calculate root hash after transaction execution
    
    Reference: https://github.com/bnb-chain/bsc/pull/2185

--- consensus/consortium/common/contract.go
+++ consensus/consortium/common/contract.go
@@ -349,6 +349,7 @@ func ApplyTransaction(msg types.Message, opts *ApplyTransactOpts) (err error) {
 		*receivedTxs = (*receivedTxs)[1:]
 	}
 	opts.State.Prepare(expectedTx.Hash(), len(*txs))
+	opts.State.SetNonce(msg.From(), nonce+1)
 	gasUsed, err := applyMessage(opts.ApplyMessageOpts, expectedTx)
 	if err != nil {
 		failed = true
@@ -378,7 +379,6 @@ func ApplyTransaction(msg types.Message, opts *ApplyTransactOpts) (err error) {
 	receipt.BlockNumber = header.Number
 	receipt.TransactionIndex = uint(opts.State.TxIndex())
 	*receipts = append(*receipts, receipt)
-	opts.State.SetNonce(msg.From(), nonce+1)
 	return nil
 }
 
