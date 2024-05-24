d6b80639705109332bb4d329cfbb346737a31d42
Bui Quang Minh <minh.bui@skymavis.com>
2024-05-24 17:07:48 +0700

    core/state: reduce memory allocation in GetLocMappingAtKey
    > go test -bench=BenchmarkGetLocMappingAtKey -benchtime=5s
    
    Before
    
    goos: linux
    goarch: amd64
    pkg: github.com/ethereum/go-ethereum/core/state
    cpu: 11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz
    BenchmarkGetLocMappingAtKey-8            8499260               877.7 ns/op           664 B/op          9 allocs/op
    
    After
    
    goos: linux
    goarch: amd64
    pkg: github.com/ethereum/go-ethereum/core/state
    cpu: 11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz
    BenchmarkGetLocMappingAtKey-8            9745719               592.2 ns/op           584 B/op          5 allocs/op

--- core/state/location_cacl.go
+++ core/state/location_cacl.go
@@ -1,6 +1,7 @@
 package state
 
 import (
+	"encoding/binary"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -13,11 +14,14 @@ func GetLocSimpleVariable(slot uint64) common.Hash {
 }
 
 func GetLocMappingAtKey(key common.Hash, slot uint64) common.Hash {
-	slotHash := common.BigToHash(new(big.Int).SetUint64(slot))
-	retByte := crypto.Keccak256(key.Bytes(), slotHash.Bytes())
-	ret := new(big.Int)
-	ret.SetBytes(retByte)
-	return common.BigToHash(ret)
+	var buffer []byte
+
+	buffer = key.Bytes()
+	// Write 8-byte slot to 32-byte space in big endian order.
+	// First write 24 0-bytes then write 8-slot in big endian.
+	buffer = common.PadTo(buffer, len(buffer)+24)
+	buffer = binary.BigEndian.AppendUint64(buffer, slot)
+	return crypto.Keccak256Hash(buffer)
 }
 
 func GetLocDynamicArrAtElement(slotHash common.Hash, index uint64, elementSize uint64) common.Hash {
