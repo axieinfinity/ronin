9d2261f592f2cfc75720c3b9b02889cc82dc96e2
Francesco4203 <100074926+Francesco4203@users.noreply.github.com>
2024-07-29 10:07:49 +0700

    consensus: Add consensus blob check (#504)
    * consensus/misc: add eip4844 verification and test
    
    - Add eip4844 to misc
    - Move eip1559 to separate folder
    
    * consensus/v2: add blob header verification
    
    - add VerifyEIP4844Header in VerifyHeader
    - add VerifyBlobHeader, and dummy functions in other engines
    - add new blob fields to encodeSigHeader
    
    * consensus/v2: add VerifyBlobHeader coverage unit tests

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -178,6 +178,7 @@ const (
 
 	BlobTxTargetBlobGasPerBlock = 3 * BlobTxBlobGasPerBlob // Target consumable blob gas for data blobs per block (for 1559-like pricing)
 	MaxBlobGasPerBlock          = 6 * BlobTxBlobGasPerBlob // Maximum consumable blob gas for data blobs per block
+	MaxBlobsPerBlock            = 6                        // Maximum number of data blobs per block
 )
 
 // Gas discount table for BLS12-381 G1 and G2 multi exponentiation operations
