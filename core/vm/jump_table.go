34db9d2dc200e7e52cfe6b901ffe0dc58e3b3469
Bui Quang Minh <minh.bui@skymavis.com>
2024-08-07 11:47:48 +0700

    core/vm: enable EIP-1153 Transient Storage in Cancun

--- core/vm/jump_table.go
+++ core/vm/jump_table.go
@@ -69,6 +69,7 @@ func newCancunInstructionSet() JumpTable {
 	instructionSet := newShanghaiInstructionSet()
 	enable4844(&instructionSet) // EIP-4844 (BLOBHASH opcode)
 	enable7516(&instructionSet) // EIP-7516 (BLOBBASEFEE opcode)
+	enable1153(&instructionSet) // EIP-1153 "Transient Storage"
 	enable5656(&instructionSet) // EIP-5656 (MCOPY opcode)
 	enable6780(&instructionSet) // EIP-6780 SELFDESTRUCT only in same transaction
 	return instructionSet
