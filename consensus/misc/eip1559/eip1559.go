37d10f7036a7643b826748e978f7ff3a4a2b174e
minh-bq <97180373+minh-bq@users.noreply.github.com>
2024-08-29 13:17:39 +0700

    consensus, core: enable base fee after Venoki fork (#543)
    * params: add Venoki hardfork definition
    
    * consensus, core: enable base fee after Venoki fork
    
    After London fork, we always set base fee to 0. This commit implements REP-0022:
    Enable base fee on Ronin
    (https://github.com/ronin-chain/REPs/blob/main/REP-0022/REP-0022.md) to enable
    base fee calculation and transfer that fee to treasury after Venoki fork.

--- consensus/misc/eip1559/eip1559.go
+++ consensus/misc/eip1559/eip1559.go
@@ -20,6 +20,8 @@ import (
 	"fmt"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/consensus/misc"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/params"
@@ -47,48 +49,53 @@ func VerifyEip1559Header(config *params.ChainConfig, parent, header *types.Heade
 
 // CalcBaseFee calculates the basefee of the header.
 func CalcBaseFee(config *params.ChainConfig, parent *types.Header) *big.Int {
-	return big.NewInt(0)
+	blockNumber := new(big.Int).Add(parent.Number, common.Big1)
 
-	/*
-		We temporarily set the base fee to 0, the following logic may be enabled in
-		the future hardfork.
+	if !config.IsVenoki(blockNumber) {
+		return big.NewInt(0)
+	}
 
-		// If the current block is the first EIP-1559 block, return the InitialBaseFee.
-		if !config.IsLondon(parent.Number) {
-			return new(big.Int).SetUint64(params.InitialBaseFee)
-		}
+	// Current block is the first block in Venoki, return initial base fee
+	if !config.IsVenoki(parent.Number) {
+		return new(big.Int).SetUint64(params.InitialBaseFee)
+	}
 
-		var (
-			parentGasTarget          = parent.GasLimit / params.ElasticityMultiplier
-			parentGasTargetBig       = new(big.Int).SetUint64(parentGasTarget)
-			baseFeeChangeDenominator = new(big.Int).SetUint64(params.BaseFeeChangeDenominator)
+	var (
+		parentGasTarget          = parent.GasLimit / params.ElasticityMultiplier
+		parentGasTargetBig       = new(big.Int).SetUint64(parentGasTarget)
+		baseFeeChangeDenominator = new(big.Int).SetUint64(params.BaseFeeChangeDenominator)
+	)
+	// If the parent gasUsed is the same as the target, the baseFee remains unchanged.
+	if parent.GasUsed == parentGasTarget {
+		return new(big.Int).Set(parent.BaseFee)
+	}
+	if parent.GasUsed > parentGasTarget {
+		// If the parent block used more gas than its target, the baseFee should increase.
+		gasUsedDelta := new(big.Int).SetUint64(parent.GasUsed - parentGasTarget)
+		x := new(big.Int).Mul(parent.BaseFee, gasUsedDelta)
+		y := x.Div(x, parentGasTargetBig)
+		baseFeeDelta := math.BigMax(
+			x.Div(y, baseFeeChangeDenominator),
+			common.Big1,
 		)
-		// If the parent gasUsed is the same as the target, the baseFee remains unchanged.
-		if parent.GasUsed == parentGasTarget {
-			return new(big.Int).Set(parent.BaseFee)
-		}
-		if parent.GasUsed > parentGasTarget {
-			// If the parent block used more gas than its target, the baseFee should increase.
-			gasUsedDelta := new(big.Int).SetUint64(parent.GasUsed - parentGasTarget)
-			x := new(big.Int).Mul(parent.BaseFee, gasUsedDelta)
-			y := x.Div(x, parentGasTargetBig)
-			baseFeeDelta := math.BigMax(
-				x.Div(y, baseFeeChangeDenominator),
-				common.Big1,
-			)
 
-			return x.Add(parent.BaseFee, baseFeeDelta)
-		} else {
-			// Otherwise if the parent block used less gas than its target, the baseFee should decrease.
-			gasUsedDelta := new(big.Int).SetUint64(parentGasTarget - parent.GasUsed)
-			x := new(big.Int).Mul(parent.BaseFee, gasUsedDelta)
-			y := x.Div(x, parentGasTargetBig)
-			baseFeeDelta := x.Div(y, baseFeeChangeDenominator)
-
-			return math.BigMax(
-				x.Sub(parent.BaseFee, baseFeeDelta),
-				common.Big0,
-			)
+		return x.Add(parent.BaseFee, baseFeeDelta)
+	} else {
+		// If the parent's base fee is at the minimum already, fast return the minimum base fee
+		minimumBaseFee := big.NewInt(params.MinimumBaseFee)
+		if parent.BaseFee.Cmp(minimumBaseFee) == 0 {
+			return minimumBaseFee
 		}
-	*/
+		// Otherwise if the parent block used less gas than its target, the baseFee should decrease.
+		gasUsedDelta := new(big.Int).SetUint64(parentGasTarget - parent.GasUsed)
+		x := new(big.Int).Mul(parent.BaseFee, gasUsedDelta)
+		y := x.Div(x, parentGasTargetBig)
+		baseFeeDelta := x.Div(y, baseFeeChangeDenominator)
+
+		// Ensure the minimum baseFee is 1 gwei.
+		return math.BigMax(
+			x.Sub(parent.BaseFee, baseFeeDelta),
+			minimumBaseFee,
+		)
+	}
 }
