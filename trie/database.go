59ec0d9ee98bd28184b8fa5ee84a236e3186d3ab
Francesco4203 <100074926+Francesco4203@users.noreply.github.com>
2024-09-26 10:22:26 +0700

    all: prep for path-based trie storage (#582)
    * all: prep for path-based trie storage
    
    * all: use rawdb.HasLegacyNode() to check for node existance instead of check for length

--- trie/database.go
+++ trie/database.go
@@ -415,7 +415,7 @@ func (db *Database) Node(hash common.Hash) ([]byte, error) {
 	memcacheDirtyMissMeter.Mark(1)
 
 	// Content unavailable in memory, attempt to retrieve from disk
-	enc := rawdb.ReadTrieNode(db.diskdb, hash)
+	enc := rawdb.ReadLegacyTrieNode(db.diskdb, hash)
 	if len(enc) != 0 {
 		if db.cleans != nil {
 			db.cleans.Set(hash[:], enc)
@@ -580,7 +580,7 @@ func (db *Database) Cap(limit common.StorageSize) error {
 	for size > limit && oldest != (common.Hash{}) {
 		// Fetch the oldest referenced node and push into the batch
 		node := db.dirties[oldest]
-		rawdb.WriteTrieNode(batch, oldest, node.rlp())
+		rawdb.WriteLegacyTrieNode(batch, oldest, node.rlp())
 
 		// If we exceeded the ideal batch size, commit and reset
 		if batch.ValueSize() >= ethdb.IdealBatchSize {
@@ -710,7 +710,7 @@ func (db *Database) commit(hash common.Hash, batch ethdb.Batch, uncacher *cleane
 		return err
 	}
 	// If we've reached an optimal batch size, commit and start over
-	rawdb.WriteTrieNode(batch, hash, node.rlp())
+	rawdb.WriteLegacyTrieNode(batch, hash, node.rlp())
 	if callback != nil {
 		callback(hash)
 	}
@@ -895,6 +895,6 @@ func (db *Database) SaveCachePeriodically(dir string, interval time.Duration, st
 }
 
 // Scheme returns the node scheme used in the database. Right now, we only support hash scheme.
-func (db *Database) Scheme() NodeScheme {
-	return &hashScheme{}
+func (db *Database) Scheme() string {
+	return rawdb.HashScheme
 }
