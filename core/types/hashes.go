742b3456c4268f46bf313df87afce619372253ab
chiphamskymavis <chi.pham@skymavis.com>
2025-01-02 17:58:26 +0700

    core/types: add empty code hash

--- core/types/hashes.go
+++ core/types/hashes.go
@@ -18,7 +18,19 @@ package types
 
 import (
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/log"
+)
+
+var (
+	// EmptyRootHash is the known root hash of an empty merkle trie.
+	EmptyRootHash = common.HexToHash("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421")
+
+	// EmptyUncleHash is the known hash of the empty uncle set.
+	EmptyUncleHash = rlpHash([]*Header(nil)) // 1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347
+
+	// EmptyCodeHash is the known hash of the empty EVM bytecode.
+	EmptyCodeHash = crypto.Keccak256Hash(nil) // c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470
 )
 
 // TrieRootHash returns the hash itself if it's non-empty or the predefined
